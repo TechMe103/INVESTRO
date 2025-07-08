65fc571f804791de3715f0782c7256511e11e87c
TechMe103 <salunkhesanika610@gmail.com>
2025-07-08 06:44:06 -0700

    Investo

---
+++ dashboard/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Dashboard from "./Dashboard";
+import TopBar from "./TopBar";
+// import Holdings from "./Holdings";
+
+const Home = () => {
+  return (
+    <>
+      <TopBar />
+      <Dashboard />
+      {/* <Holdings /> */}
+    </>
+  );
+};
+
+export default Home;
